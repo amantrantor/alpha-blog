929ec2005a5ddd5f4ae5cae53fbe1a17c191c2a4
Aman Kashyap <aman.kashyap@trantorinc.com>
2022-07-12 17:44:53 +0530

    12/07/2022 work

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
+
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
     def show
-        @article = Article.find(params[:id])
+       
 
 
     end
@@ -8,6 +10,51 @@ class ArticlesController < ApplicationController
     def index
 
         @articles = Article.all
+    end
+
+    def new
+
+        @article = Article.new
+
+    end
+    def edit
+       
+
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            redirect_to @article
+       else
+        render 'new'
+           
+       end
+    end
+
+    def update
+        
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+
+        else
+            render 'edit'
+        end
+    end 
+
+    def destroy
+        
+        @article.destroy
+        redirect_to articles_path
+
+    end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+
     end
 
 
