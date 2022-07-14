bb886c3a16dae7f742312d5a421df1483e0543e4
Aman Kashyap <aman.kashyap@trantorinc.com>
2022-07-14 18:01:42 +0530

    user-articles-association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.user = User.first
         if @article.save
             redirect_to @article
        else
