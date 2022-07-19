b662b7d967a452ab4374dc792a062896e6d9750c
Aman Kashyap <aman.kashyap@trantorinc.com>
2022-07-19 12:23:01 +0530

    sign-up completion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,17 @@ class UsersController < ApplicationController
         @user = User.new
     end
     def create
-        byebug
-    end
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully sign up"
+            redirect_to articles_path
+        else
+            render 'new'
+        end
 
+    end
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+
+    end
 end
